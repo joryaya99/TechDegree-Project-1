ff650ccf8f4c051a05312c4cb7a4f0c6938b7d15
joryaya99 <110695145+joryaya99@users.noreply.github.com>
2022-10-18 20:28:04 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,37 +1,24 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
 import random
 
+number = random.randint(1, 11)
+
+attempts = 0
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-
-
-
-# Kick off the program by calling the start_game function.
-start_game()
+  attempts = 0
+  print("Welcome to the Guessing Game!")
+  
+while True:
+  try:
+    guess = int(input("Guess: "))
+  except ValueError:
+    print("Guess with numbers please")  
+  if guess > number:
+    print("Go lower")
+    attempts += 1
+  elif guess < number:
+    print("Go higher")
+    attempts += 1
+  elif guess == number:
+    break    
+print("Got it!")
