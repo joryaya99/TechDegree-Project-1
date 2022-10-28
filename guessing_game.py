7ac0221e1d00fa35d124e29226403f9a6d58873f
joryaya99 <110695145+joryaya99@users.noreply.github.com>
2022-10-28 12:33:19 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,24 +1,33 @@
 import random
 
-number = random.randint(1, 11)
-
-attempts = 0
+player_score = []
+print("Welcome to the Guessing Game!")
 
 def start_game():
-  attempts = 0
-  print("Welcome to the Guessing Game!")
-  
+	number = random.randint(1, 11)
+	attempts = 0
+
 while True:
   try:
     guess = int(input("Guess: "))
   except ValueError:
-    print("Guess with numbers please")  
+    print("Guess with numbers please")
+		continue
+	if guess > 10:
+		print("Can't be over 10")
+		continue
+	if user_guess < 1:
+		print("Can't be under 1")
+		continue
   if guess > number:
     print("Go lower")
     attempts += 1
-  elif guess < number:
+  if guess < number:
     print("Go higher")
     attempts += 1
   elif guess == number:
-    break    
-print("Got it!")
+		attempts +=1
+		print("You got it, it took you {} guesses".format(attempts))
+		player_score.append(attempts)
+		player_score.sort()
+    break
