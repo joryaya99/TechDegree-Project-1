e59dd71963559d8ded3931eb8f16017e518ed600
joryaya99 <110695145+joryaya99@users.noreply.github.com>
2022-11-04 21:44:19 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,30 +1,38 @@
 import random
-	
+
 player_score = []
-print("Welcome to the Guessing Game!")
+print("Welcome to the number guessing game!!")
+ 
 
 def start_game():
-	while True:
-		number = random.randint(1, 10)
-		attempts = 0
-	try:
-		guess = int(input("Guess: "))
-	except ValueError:
-		print("Guess with numbers please")
-	if guess > 10:
-		print("Can't be over 10")
-	if guess < 1:
-		print("Can't be under 1")
-	if guess > number:
-		attempts += 1
-		print("Go lower")
-	if guess < number:
-		attempts += 1
-		print("Go higher")
-	if guess == number:
-		attempts +=1
-		print("You got it, it took you {} guesses".format(attempts))
-		
-	player_score.append(attempts)
-	player_score.sort()
-	break
+
+
+    number = random.randint(1, 10)
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Guess: "))
+        except ValueError:
+            print("Guess with numbers please")
+            continue
+        if guess > 10:
+            print("Can't be over 10")
+            continue
+        if guess < 1:
+            print("Can't be under 1")
+            continue
+        if guess > number:
+            attempts += 1
+            print("Go lower")
+        if guess < number:
+            attempts += 1
+            print("Go higher")
+        if guess == number:
+            attempts += 1
+            print("You got it, it took you {} guesses".format(attempts))
+            player_score.append(attempts)
+            player_score.sort()                                  
+            break
+
+start_game()
